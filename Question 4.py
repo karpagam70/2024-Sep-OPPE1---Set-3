793722bce032618cae76d7b8b5b91674dd34d59c
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 17:49:34 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,20 @@
+'''
+Last Word in a Given Sentence That Starts with Uppercase Letter
+Given a sentence, find the last word that starts with an uppercase letter. If no such word exists, return None. Words in the sentence are separated by spaces. Assume no punctuations will be present.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+
+    • "This is a Test sentence" --> "Test" last word that starts with an uppercase letter.
+
+"no uppercase words here", the result should be None.
+
+Explanation
+
+    • Words are separated by spaces, so we split the sentence into words.
+
+    • We check each word to see if it starts with an uppercase letter.
+
+    • We keep track of the last word that meets this condition.
+'''
