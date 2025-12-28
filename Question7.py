fa2e1e741676eb21f85b8e7c63e691118bdf2d79
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 17:52:00 +0530

    Create Question7.py

---
+++ Question7.py
@@ -0,0 +1,38 @@
+'''
+Pattern Print - N
+Given an integer n (where n > 0), print an "N" shaped pattern with n rows. The pattern should have vertical bars (|) on the left and right sides of each row, with a backslash (\) moving diagonally from the top-left to the bottom-right. There should be no spaces to the right of the pattern.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+    • A single integer n, representing the number of rows in the pattern.
+
+Output Format
+
+    • An "N" shaped pattern with n rows, as described.
+
+Examples
+
+Input:
+
+1
+Output:
+
+|\|
+Input:
+
+2
+Output:
+
+|\ |
+| \|
+Input:
+
+3
+Output:
+
+|\  |
+| \ |
+|  \|
+'''
