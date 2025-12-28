c7e6d5fe1d2f2d4a2a4439f96a3e69d57a7d6a46
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 17:47:33 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,10 @@
+'''
+Convert date from mm-dd-yy to yy-dd-mm
+Convert a date string from the format mm-dd-yy to yy-dd-mm.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+Input: "12-25-21"
+Output: "21-25-12"
+'''
